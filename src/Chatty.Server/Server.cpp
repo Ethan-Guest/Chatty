12215856cbcb6ec91176385ba5a2832fcf2d051c
Ethan <eguest01@gmail.com>
2022-08-17 08:38:00 -0500

    setup initial server console input

--- src/Chatty.Server/Server.cpp
+++ src/Chatty.Server/Server.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
+void readInput(std::string input);
+
 int main()
 {
-    std::cout << "Hello Server!\n";
+    // display initial startup messages
+    std::cout << "[SERVER READY]!\n";
+    std::cout << "To start the server use '--start'\n";
+    std::cout << "To exit the server use '--exit'\n";
+
+    // store user input
+    std::string input;
+    
+    while(true)
+    {
+        
+        std::cin >> input;
+        
+        if (input == "--start")
+        {
+            //start();
+        }
+        else if (input == "--exit")
+        {
+            return 0;
+        }
+        else
+        {
+            std::cout << "'" << input << "'" << " is not a recognized command.\n";
+        }
+    }
+
+
 }
+
+
