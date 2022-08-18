409921d4a900478c9d452d45c7bb73d1054a31e2
Ethan <eguest01@gmail.com>
2022-08-18 11:14:40 -0500

    implement multiplexing

--- src/Chatty.Client/Client.cpp
+++ src/Chatty.Client/Client.cpp
@@ -11,6 +11,45 @@
 uint16_t port = 31337;
 const char* address = "127.0.0.1";
 
+int tcp_send_whole(SOCKET s, const char* data, uint16_t length);
+int tcp_recv_whole(SOCKET s, char* buf, int len);
+
+// loop send func
+int tcp_send_whole(SOCKET s, const char* data, uint16_t length)
+{
+	int result;
+	int bytesSent = 0;
+
+	while (bytesSent < length)
+	{
+		result = send(s, (const char*)data + bytesSent, length - bytesSent, 0);
+
+		if (result <= 0)
+			return result;
+
+		bytesSent += result;
+	}
+
+	return bytesSent;
+}
+
+// loop recv func
+int tcp_recv_whole(SOCKET s, char* buf, int len)
+{
+	int total = 0;
+
+	do
+	{
+		int ret = recv(s, buf + total, len - total, 0);
+		if (ret < 1)
+			return ret;
+		else
+			total += ret;
+
+	} while (total < len);
+
+	return total;
+}
 
 int main()
 {
@@ -53,6 +92,19 @@ int main()
 	std::cout << "[" << "00:00:00" << "]" << " COMMUNICATION SOCKET CONNECTED\n";
 
 
+	//Communication
+	uint8_t size = 30;
+	char sendbuffer[30];
+	memset(sendbuffer, 0, 30);
+	strcpy(sendbuffer, "I'm a message from the client");
+
+	int result = tcp_send_whole(communicationSocket, (char*)&size, 1);
 
 
+	// close sockets
+	shutdown(communicationSocket, SD_BOTH);
+	closesocket(communicationSocket);
+
+
+	ExitProcess(EXIT_SUCCESS);
 }
--- src/Chatty.Server/Server.cpp
+++ src/Chatty.Server/Server.cpp
@@ -8,63 +8,152 @@
 #include <Windows.h>
 #include <iostream>
 
+// loop send func
+int tcp_send_whole(SOCKET skSocket, const char* data, uint16_t length)
+{
+    int result;
+    int bytesSent = 0;
 
+    while (bytesSent < length)
+    {
+        result = send(skSocket, data + bytesSent, length - bytesSent, 0);
+
+        if (result <= 0)
+            return result;
+
+        bytesSent += result;
+    }
+
+    return bytesSent;
+}
+
+// loop recv func
+int tcp_recv_whole(SOCKET s, char* buf, int len)
+{
+    int total = 0;
+
+    do
+    {
+        int ret = recv(s, buf + total, len - total, 0);
+        if (ret < 1)
+            return ret;
+        total += ret;
+    }
+    while (total < len);
+
+    return total;
+}
 
 int main()
 {
     // Initialize winsock
-	WSADATA wsadata;
-	if (WSAStartup(WINSOCK_VERSION, &wsadata) != 0)
-	{
-		// Winsock startup error
-	}
-	
-	std:: cout << "[" << "00:00:00" << "]" << " STARTUP SUCCESSFUL\n";
+    WSADATA wsadata;
+    if (WSAStartup(WINSOCK_VERSION, &wsadata) != 0)
+    {
+        // Winsock startup error
+    }
 
-	// Create a listening socket
-	SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (listenSocket == INVALID_SOCKET)
-	{
-		// Socket creation error
-	}
+    std::cout << "[" << "00:00:00" << "]" << " STARTUP SUCCESSFUL\n";
 
-	std::cout << "[" << "00:00:00" << "]" << " LISTENING SOCKET CREATED\n";
+    // Create a listening socket
+    SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (listenSocket == INVALID_SOCKET)
+    {
+        // Socket creation error
+    }
 
+    std::cout << "[" << "00:00:00" << "]" << " LISTENING SOCKET CREATED\n";
 
-	// Initialize socket address
-	SOCKADDR_IN serverAddr;
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.S_un.S_addr = INADDR_ANY;
-	serverAddr.sin_port = htons(31337);
+    // Initialize socket address
+    SOCKADDR_IN serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.S_un.S_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(31337);
 
+    // Bind
+    if (bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) != 0)
+    {
+        // Binding error
+    }
 
-	// Bind
-	if (bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) != 0)
-	{
-		// Binding error
-	}
-
-	std::cout << "[" << "00:00:00" << "]" << " LISTENING SOCKET BINDING SUCCESSFUL\n";
+    std::cout << "[" << "00:00:00" << "]" << " LISTENING SOCKET BINDING SUCCESSFUL\n";
 
 
-	// Listen
-	if (listen(listenSocket, 1) != 0)
-	{
-		// Listening error
-	}
+    // Listen
+    if (listen(listenSocket, 1) != 0)
+    {
+        // Listening error
+    }
 
-	std::cout << "[" << "00:00:00" << "]" << " SERVER IS LISTENING\n";
+    std::cout << "[" << "00:00:00" << "]" << " SERVER IS LISTENING\n";
 
-
-	// Accept
-	SOCKET communicationSocket = accept(listenSocket, NULL, NULL);
-	if (communicationSocket == INVALID_SOCKET)
-	{
-		// Socket creation error
-	}
-	std::cout << "[" << "00:00:00" << "]" << " CLIENT ACCEPTED\n";
+    FD_SET masterSet, readySet;
+    FD_ZERO(&masterSet);
+    FD_SET(listenSocket, &masterSet);
+    FD_ZERO(&readySet);
 
 
+    while (true)
+    {
+        readySet = masterSet;
+
+        int readySockets = select(0, &readySet, nullptr, nullptr, nullptr);
+
+        for (int i = 0; i < readySockets; i++)
+        {
+            SOCKET communicationSocket = readySet.fd_array[i];
+            if (communicationSocket == listenSocket)
+            {
+                // accept a new connection
+                SOCKET client = accept(listenSocket, nullptr, nullptr);
+
+                // add the new connection to the list of connected clients
+                FD_SET(client, &masterSet);
+
+                std::cout << "[" << "00:00:00" << "]" << " NEW CLIENT CONNECTION\n";
+
+                // send feedback to client
+                uint8_t size = 24;
+                char sendbuffer[24];
+                memset(sendbuffer, 0, 24);
+                strcpy(sendbuffer, "[CONNECTED TO SERVER]: ");
+
+                int result = tcp_send_whole(client, (char*)&size, 1);
+
+                result = tcp_send_whole(client, sendbuffer, size);
+            }
+            else
+            {
+                uint8_t messageSize = 0;
+
+                auto buffer = new char[messageSize];
+
+                // TODO: DUPLICATE CODE, COMPACT INTO ONE FUNCTION WITH SIZE PARAMETER
+                // accept new message
+                int bytes = tcp_recv_whole(communicationSocket, (char*)&messageSize, 1);
+                if (bytes <= 0)
+                {
+                    // Drop the client
+                    closesocket(communicationSocket);
+                    FD_CLR(communicationSocket, &masterSet);
+                    std::cout << "[" << "00:00:00" << "]" << " CLIENT " << communicationSocket << " DISCONNECTED\n";
+                    continue;
+                }
+
+                bytes = tcp_recv_whole(communicationSocket, buffer, messageSize);
+                if (bytes <= 0)
+                {
+                    std::cout << "[" << "00:00:00" << "]" << " MESSAGE COULD NOT BE RECEIVED\n";
+                }
+                else
+                {
+                    std::cout << communicationSocket << buffer << "\n";
+                }
+            }
+        }
+    }
+
+    WSACleanup();
 
     ExitProcess(EXIT_SUCCESS);
 }
