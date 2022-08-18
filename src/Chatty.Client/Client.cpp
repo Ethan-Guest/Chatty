cb8e3ef22ea096172b34983303bbbd345e14afe7
Ethan <eguest01@gmail.com>
2022-08-17 19:44:09 -0500

    create client and server TCP connection

--- src/Chatty.Client/Client.cpp
+++ src/Chatty.Client/Client.cpp
@@ -1,6 +1,58 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS
+
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
 #include <iostream>
+
+uint16_t port = 31337;
+const char* address = "127.0.0.1";
+
 
 int main()
 {
-    std::cout << "Hello Client!\n";
+	// Initialize winsock
+	WSADATA wsadata;
+	if (WSAStartup(WINSOCK_VERSION, &wsadata) != 0)
+	{
+		// Winsock startup error
+	}
+
+	std::cout << "[" << "00:00:00" << "]" << " STARTUP SUCCESSFUL\n";
+
+
+	// Create a communication socket
+	SOCKET communicationSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (communicationSocket == INVALID_SOCKET)
+	{
+		// Socket creation error
+	}
+
+	std::cout << "[" << "00:00:00" << "]" << " COMMUNICATION SOCKET CREATED\n";
+
+
+	// Initialize socket address
+	SOCKADDR_IN clientAddr;
+	clientAddr.sin_family = AF_INET;
+	clientAddr.sin_addr.S_un.S_addr = inet_addr(address);
+	if (clientAddr.sin_addr.S_un.S_addr == INADDR_NONE)
+	{
+		// Address not in dotted-quadrant format
+	}
+	clientAddr.sin_port = htons(port);
+
+	// Connect
+	if (connect(communicationSocket, (SOCKADDR*)&clientAddr, sizeof(clientAddr)) != 0)
+	{
+		// Connect error
+	}
+
+	std::cout << "[" << "00:00:00" << "]" << " COMMUNICATION SOCKET CONNECTED\n";
+
+
+
+
 }
--- src/Chatty.Server/Server.cpp
+++ src/Chatty.Server/Server.cpp
@@ -1,37 +1,70 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS
+
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
 #include <iostream>
 
-void readInput(std::string input);
+
 
 int main()
 {
-    // display initial startup messages
-    std::cout << "[SERVER READY]!\n";
-    std::cout << "To start the server use '--start'\n";
-    std::cout << "To exit the server use '--exit'\n";
+    // Initialize winsock
+	WSADATA wsadata;
+	if (WSAStartup(WINSOCK_VERSION, &wsadata) != 0)
+	{
+		// Winsock startup error
+	}
+	
+	std:: cout << "[" << "00:00:00" << "]" << " STARTUP SUCCESSFUL\n";
 
-    // store user input
-    std::string input;
-    
-    while(true)
-    {
-        
-        std::cin >> input;
-        
-        if (input == "--start")
-        {
-            //start();
-        }
-        else if (input == "--exit")
-        {
-            return 0;
-        }
-        else
-        {
-            std::cout << "'" << input << "'" << " is not a recognized command.\n";
-        }
-    }
+	// Create a listening socket
+	SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		// Socket creation error
+	}
+
+	std::cout << "[" << "00:00:00" << "]" << " LISTENING SOCKET CREATED\n";
 
 
+	// Initialize socket address
+	SOCKADDR_IN serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.S_un.S_addr = INADDR_ANY;
+	serverAddr.sin_port = htons(31337);
+
+
+	// Bind
+	if (bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) != 0)
+	{
+		// Binding error
+	}
+
+	std::cout << "[" << "00:00:00" << "]" << " LISTENING SOCKET BINDING SUCCESSFUL\n";
+
+
+	// Listen
+	if (listen(listenSocket, 1) != 0)
+	{
+		// Listening error
+	}
+
+	std::cout << "[" << "00:00:00" << "]" << " SERVER IS LISTENING\n";
+
+
+	// Accept
+	SOCKET communicationSocket = accept(listenSocket, NULL, NULL);
+	if (communicationSocket == INVALID_SOCKET)
+	{
+		// Socket creation error
+	}
+	std::cout << "[" << "00:00:00" << "]" << " CLIENT ACCEPTED\n";
+
+
+
+    ExitProcess(EXIT_SUCCESS);
 }
-
-
